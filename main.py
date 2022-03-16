29b497960cbfea865add094c6a1e8767ca9e38ad
Ryan Nguyen <vastmine1029@gmail.com>
2022-03-16 07:29:42 -0700

    updated code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask # import from Flask
+from flask import Flask, redirect, url_for # import from Flask
 
 app = Flask(__name__)
 
 @app.route("/") # "/" means default page. In this case, the homepage.
 def home():
-    return "Homepage bruv"
+    return "<h1>HOMEPAGE</h1>Homepage bruv"
 
 if __name__ == "__main__":
     app.run() #runs program
