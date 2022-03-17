05bf88f5fb4456183fcb2d51fd80b75ec05d3f88
Jose Guadalupe Hernandez <jose.g.hernandez@sjsu.edu>
2022-03-16 19:10:57 -0700

    Html page linked from python code

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/") # "/" means default page. In this case, the homepage.
 def home():
-    return "<div style=\"color: red; font-size: 16pt;\">HOMEPAGE</div><br/>Homepage bruv" # using HTML to stylize text
+    import webbrowser
+    webbrowser.open('index-page.html') 
+    
+    #return "<div style=\"color: red; font-size: 16pt;\">HOMEPAGE</div><br/>Homepage bruv" # using HTML to stylize text
 
 if __name__ == "__main__":
     app.run() #runs program
