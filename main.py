ef631dd120c195f88250153b48d0817bbdf07bb0
Ryan Nguyen <vastmine1029@gmail.com>
2022-03-16 20:59:19 -0700

    updated format

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, url_for # import from Flask
+import webbrowser
 
 app = Flask(__name__)
 
 @app.route("/") # "/" means default page. In this case, the homepage.
 def home():
-    import webbrowser
     webbrowser.open('index-page.html') 
     
     #return "<div style=\"color: red; font-size: 16pt;\">HOMEPAGE</div><br/>Homepage bruv" # using HTML to stylize text
