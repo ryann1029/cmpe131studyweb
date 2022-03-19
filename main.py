e427a320b98b5f05e2fd3372abce5bc9c4302cb8
Ryan Nguyen <vastmine1029@gmail.com>
2022-03-19 01:17:34 -0700

    minor update

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from flask import Flask, redirect, url_for, render_template # import from Flask
+import webbrowser
 
 app = Flask(__name__)
 
 @app.route("/") # "/" means default page. In this case, the homepage.
 @app.route("/home") # 
 def home():
-    #webbrowser.open('index-page.html') # This renders template but affects URL...
-    return render_template("index-page.html") #using render_template so that it renders the content ot the HTML and does no not affect URL
-    
-    #return "<div style=\"color: red; font-size: 16pt;\">HOMEPAGE</div><br/>Homepage bruv" # using HTML to stylize text
-
-'''@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"'''
+    return render_template("home.html") #using render_template so that it renders the content ot the HTML and does no not affect URL
 
 @app.route("/page1")
+def page1():
+    return render_template("page1.html")
+
 @app.route("/page2")
-@app.route("/page2")
+@app.route("/page3")
 @app.route("/aboutus")
 @app.route("/purpose")
 def page():
     return redirect(url_for("home"))
+
+@app.route("/<name>")
+def user(name):
+    return render_template("error404.html")
 
 if __name__ == "__main__":
     app.run(debug = True) #runs program. every time you save any file, the new version runs automatically in console.
