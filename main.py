6ecbd17c34530c7d9a8652a8adadf14123f712be
Ryan Nguyen <vastmine1029@gmail.com>
2022-03-19 02:38:58 -0700

    Commented and organized code

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+####### IMPORTS #######
 from flask import Flask, redirect, url_for, render_template # import from Flask
-import webbrowser
 
 app = Flask(__name__)
 
+
+
+
+
+####### ACTIVE PAGES #######
 @app.route("/") # "/" means default page. In this case, the homepage.
 @app.route("/home") # 
 def home():
@@ -12,16 +17,30 @@ def home():
 def page1():
     return render_template("page1.html")
 
-@app.route("/page2")
-@app.route("/page3")
 @app.route("/aboutus")
-@app.route("/purpose")
-def page():
-    return redirect(url_for("home"))
+def about_us():
+    return render_template("about_us.html")
 
+# Any pages that don't exist goes to "error404.html"
 @app.route("/<name>")
 def user(name):
     return render_template("error404.html")
 
+
+
+
+
+####### INACTIVE PAGES - redirects to "home.html" #######
+@app.route("/page2")
+@app.route("/page3")
+@app.route("/purpose")
+def page():
+    return redirect(url_for("home"))
+
+
+
+
+
+# Run Program
 if __name__ == "__main__":
-    app.run(debug = True) #runs program. every time you save any file, the new version runs automatically in console.
+    app.run(debug = True) # runs program. every time you save any file, the new version runs automatically in console.
