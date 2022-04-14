c90a3bebbcf2a790c981b71ce0cc8022c8778f55
Ryan Nguyen <vastmine1029@gmail.com>
2022-04-14 03:12:53 -0700

    added more conditions

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -1,9 +1,9 @@
-const isAlpha = str => /^[a-zA-Z0-9]*$/.test(str); // Using RegEx to detect any letters.
+const isAlpha = str => /^([a-zA-Z0-9]|\s)*$/.test(str); // Using RegEx to detect any letters.
 
 const calculate = () => {
   
     // Getting input from user into height variable.
-    let course1 = document.querySelector("#course1").value;
+    let course1 = document.querySelector("#assignment").value;
     let score = document.querySelector("#score").value;
     let total_points = document.querySelector("#totalpoints").value;
     let weight = document.querySelector("#weight").value; 
@@ -29,25 +29,37 @@ const calculate = () => {
       grades = "B";
     } else if (percentage < 80 && percentage >= 70) {
       grades = "C";
+    } else if (percentage < 70 && percentage >= 60) {
+      grades = "D";
     } else {
       grades = "F";
     }
 
-    // Checking if inputs is either empty, contains alphabets, or something otherwise.
+    // Checking if inputs is either empty, contains characters not part of the alphabet, or something otherwise.
     if (course1 == "") {
     document.querySelector("#showdata").innerHTML
         = `Please enter all the fields`;
         
-    } else if(isAlpha(course1)){
+    } else if(!isAlpha(course1)){
         document.querySelector("#showdata").innerHTML =
-        `No special charactersare allowed for course name. Correct your inputs.`
+        `No special characters are allowed for course name. Correct your inputs.`
+
+    } else if (!weight) {
+      document.querySelector("#showdata").innerHTML =
+      `No weight inputted.`
+
+    } else if (!score) {
+      document.querySelector("#showdata").innerHTML =
+      `No score inputted.`
+
+    } else if (!total_points) {
+      document.querySelector("#showdata").innerHTML =
+      `No total points inputted.`
 
     } else {
     // Checking the condition for the fail and pass
-        if (percentage >= 39.5) {
-            document.querySelector(
-            "#showdata"
-            ).innerHTML = 
+        if (percentage >= 70) {
+            document.querySelector("#showdata").innerHTML = 
             `You have ${course1_points} points out of ${total_points}, which is a percentage of ${percentage}%.<br/> 
             <b>Your grade is ${grades}, <u>a passing grade</u></b>.`;
 
@@ -58,4 +70,4 @@ const calculate = () => {
         }
     }
   }
-};  
+};
